be3fc78aeac96729dae1fccdfdb4b858d297b37b
xlhLucca <ana.julia.ignacio@escola.pr.gov.br>
2025-05-15 19:48:11 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,40 @@
-let x
-let y
+let cor;
+let posicaoHorizontal; // x
+let posicaoVertical; // y
 
 function setup() {
   createCanvas(400, 400);
-  background('red');
-  x = random(400);
-  y = random(400);
+  background("white");
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
 }
 
 function draw() {
- circle(x, y, 10);
- 
-  if (mouseX == x) {
-    text('Te encontrei!', 200, 200);
+  // Preenche o círculo com a cor aleatória
+  fill(cor);
+
+  // Desenha o círculo na posição aleatória
+  circle(posicaoHorizontal, posicaoVertical, 50);
+
+  // Movimento horizontal
+  if (mouseX < posicaoHorizontal) {
+    posicaoHorizontal--; // Move para a esquerda
+  }
+  if (mouseX > posicaoHorizontal) {
+    posicaoHorizontal++; // Move para a direita
   }
 
+  // Movimento vertical
+  if (mouseY < posicaoVertical) {
+    posicaoVertical--; // Move para cima
+  }
+  if (mouseY > posicaoVertical) {
+    posicaoVertical++; // Move para baixo
+  }
+
+  // Se o mouse estiver pressionado, altera a cor
+  if (mouseIsPressed) {
+    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+  }
+}
